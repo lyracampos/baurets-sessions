0411d08b40d4fd8873bfc9a2b1d5752e1765fc6d
Guilherme Lyra <lyracampos@gmail.com>
2024-04-11 22:02:42 -0300

    add linter

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
+GOLANGCI_LINT := go run github.com/golangci/golangci-lint/cmd/golangci-lint@v1.51.1
+
 build:
 	go build ./...
 	
 test:
 	go test ./... -v -cover
+
+lint:
+	$(GOLANGCI_LINT) run --fix
--- main.go
+++ main.go
@@ -10,16 +10,13 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-
 	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "pong v1.0.1")
 	}).Methods(http.MethodGet)
-
 	srv := &http.Server{
 		Addr:    "0.0.0.0:8080",
 		Handler: r,
 	}
-
 	if err := srv.ListenAndServe(); err != nil {
 		log.Println(err)
 	}
@@ -30,6 +27,5 @@ func sum(val1, val2 int) int {
 	if total <= 0 {
 		return 0
 	}
-
 	return total
 }
